8d1c75fea75e7ecbac17b1b4e11b846060c4f6d7
@hexlet/cli <support@hexlet.io>
2024-03-06 09:21:44 +0300

    submit java-web-ru/template

--- java-web-ru/template/build.gradle.kts
+++ java-web-ru/template/build.gradle.kts
@@ -5,7 +5,7 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 plugins {
     id("com.github.ben-manes.versions") version "0.48.0"
     application
-    
+    id("io.freefair.lombok") version "8.1.0"
 }
 
 application {
@@ -21,13 +21,19 @@ repositories {
 
 dependencies {
     implementation("io.javalin:javalin:5.6.1")
+
+    // BEGIN
+    implementation("gg.jte:jte:3.0.1")
+    implementation("io.javalin:javalin-rendering:5.6.2")
+    // END
+
+    implementation("io.javalin:javalin-bundle:5.6.2")
     implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("net.datafaker:datafaker:2.0.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("com.konghq:unirest-java:3.13.0")
-    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.assertj:assertj-core:3.23.1")
 }
 
 tasks.test {
@@ -41,4 +47,3 @@ tasks.test {
         showStandardStreams = true
     }
 }
-
