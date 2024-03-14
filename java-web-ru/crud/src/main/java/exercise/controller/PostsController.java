f52d2a84820d003476210c0918fa8acd1115d612
@hexlet/cli <support@hexlet.io>
2024-03-14 13:23:14 +0300

    submit java-web-ru/crud

--- java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
+++ java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
@@ -16,19 +16,23 @@ public class PostsController {
     // BEGIN
     public static void index(Context ctx) {
         var term = ctx.queryParam("page") == null ? "1" : ctx.queryParam("page");
+        var allPosts = PostRepository.getEntities();
         int num = Integer.parseInt(term);
-        var posts = PostRepository.getEntities().stream()
+
+        var posts = allPosts
+                .stream()
                 .sorted(Comparator.comparingLong(Post::getId))
                 .skip(num * 5L)
                 .limit(5)
                 .collect(Collectors.toList());
+
         var page = new PostsPage(posts, term);
         ctx.render("posts/index.jte", Collections.singletonMap("page", page));
     }
 
     public static void show(Context ctx) {
         var id = ctx.pathParamAsClass("id", Long.class).get();
-        Post post = PostRepository.find(id).orElseThrow(NotFoundResponse::new);
+        var post = PostRepository.find(id);
 
         if (Objects.equals(null, post)) {
             throw new NotFoundResponse("Page with id = " + id + " not found");
--- java-web-ru/crud/src/main/java/exercise/dto/posts/PostPage.java
+++ java-web-ru/crud/src/main/java/exercise/dto/posts/PostPage.java
@@ -5,8 +5,10 @@ import exercise.model.Post;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Optional;
+
 @AllArgsConstructor
 @Getter
 public class PostPage {
-    private Post post;
+    private Optional<Post> post;
 }
