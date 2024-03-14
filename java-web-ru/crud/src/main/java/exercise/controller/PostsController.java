8981b0973f4c6972f78a1607336a97ae09cb97ba
@hexlet/cli <support@hexlet.io>
2024-03-14 13:05:19 +0300

    submit java-web-ru/crud

---
+++ java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,42 @@
+package exercise.controller;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import exercise.dto.posts.PostsPage;
+import exercise.dto.posts.PostPage;
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+
+public class PostsController {
+
+    // BEGIN
+    public static void index(Context ctx) {
+        var term = ctx.queryParam("page") == null ? "1" : ctx.queryParam("page");
+        int num = Integer.parseInt(term);
+        var posts = PostRepository.getEntities().stream()
+                .sorted(Comparator.comparingLong(Post::getId))
+                .skip(num * 5L)
+                .limit(5)
+                .collect(Collectors.toList());
+
+        var page = new PostsPage(posts, term);
+        ctx.render("posts/index.jte", Collections.singletonMap("page", page));
+    }
+
+    public static void show(Context ctx) {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        Post post = PostRepository.find(id).orElseThrow(NotFoundResponse::new);
+
+        if (Objects.equals(null, post)) {
+            throw new NotFoundResponse("Page with id = " + id + " not found");
+        }
+
+        var page = new PostPage(post);
+        ctx.render("posts/show.jte", Collections.singletonMap("page", page));
+    }
+    // END
+}
---
+++ java-web-ru/crud/src/main/java/exercise/util/NamedRoutes.java
@@ -0,0 +1,18 @@
+package exercise.util;
+
+public class NamedRoutes {
+
+    public static String rootPath() {
+        return "/";
+    }
+
+    // BEGIN
+    public static String indexPath() {
+        return "/post";
+    }
+
+    public static String showPath() {
+        return "/post/build";
+    }
+    // END
+}
