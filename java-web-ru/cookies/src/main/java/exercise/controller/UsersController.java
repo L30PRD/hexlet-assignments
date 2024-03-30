3f0a4ed2d3fd88678c9ce0704edff4a29faa1364
@hexlet/cli <support@hexlet.io>
2024-03-30 07:44:51 +0300

    submit java-web-ru/cookies

--- java-web-ru/cookies/src/main/java/exercise/controller/UsersController.java
+++ java-web-ru/cookies/src/main/java/exercise/controller/UsersController.java
@@ -1,16 +1,11 @@
 package exercise.controller;
 
-import org.apache.commons.lang3.StringUtils;
-import exercise.util.Security;
 import exercise.model.User;
-import exercise.util.NamedRoutes;
-import static io.javalin.rendering.template.TemplateUtil.model;
 import exercise.repository.UserRepository;
-import exercise.dto.users.UserPage;
-import io.javalin.http.NotFoundResponse;
+import exercise.util.NamedRoutes;
+import exercise.util.Security;
 import io.javalin.http.Context;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
 
